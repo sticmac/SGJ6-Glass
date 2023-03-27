57236dcec6783307c1a1d96fcd88bcb8b10394e9
Julien Lemaire <julien.lemaire@sticmac.fr>
2023-03-27 12:27:07 +0200

    feat: Dialog message displays progressively

--- Assets/Scripts/UI/DialogUI.cs
+++ Assets/Scripts/UI/DialogUI.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -15,8 +16,12 @@ public class DialogUI : MonoBehaviour
     [SerializeField] TextMeshProUGUI _authorText;
     [SerializeField] TextMeshProUGUI _messageText;
 
+    [Header("Parameters")]
+    [SerializeField, Tooltip("Speed at which the message is displayed")] float _messageDisplaySpeed = 1f;
+
     private Dialog _currentDialog;
     private IEnumerator<DialogElement> _dialogEnumerator;
+    private bool _isWrittingMessage = false;
 
     public void Init(Dialog dialog)
     {
@@ -29,12 +34,19 @@ public class DialogUI : MonoBehaviour
     // Display next dialog element
     public void Next()
     {
-        if (_dialogEnumerator.MoveNext())
+        if (_isWrittingMessage)
         {
+            // orders to finish writting the currently displayed message
+            _isWrittingMessage = false;
+        }
+        else if (_dialogEnumerator.MoveNext())
+        {
+            // goes to next dialog element
             DisplayDialogElement(_dialogEnumerator.Current);
         }
         else
         {
+            // the dialog is finished
             _uiWindowActivator.Hide();
             _currentDialog.End();
         }
@@ -43,6 +55,22 @@ public class DialogUI : MonoBehaviour
     private void DisplayDialogElement(DialogElement element)
     {
         _authorText.text = element.Author.Name;
-        _messageText.text = element.Message;
+        _isWrittingMessage = true;
+        StartCoroutine(DisplayDialogMessageCoroutine(element.Message));
+    }
+
+    private IEnumerator DisplayDialogMessageCoroutine(string message)
+    {
+        float currentMessagePosition = 0;
+
+        while (_isWrittingMessage && currentMessagePosition < message.Length)
+        {
+            _messageText.text = message.Substring(0, ((int)currentMessagePosition));
+            yield return null;
+            currentMessagePosition += _messageDisplaySpeed * Time.deltaTime;
+        }
+
+        _messageText.text = message;
+        _isWrittingMessage = false;
     }
 }
